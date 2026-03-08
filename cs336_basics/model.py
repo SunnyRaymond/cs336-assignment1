8d0a4fb2f48a43486ed8dc184c739099882eb177
Raymond Guo <yguo017@e.ntu.edu.sg>
2026-03-08 11:48:23 +0800

    update model.py and adapters.py

--- cs336_basics/model.py
+++ cs336_basics/model.py
@@ -26,3 +26,26 @@ class Linear(nn.Module):
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return torch.matmul(x, self.W.transpose(-1, -2))
+
+
+class Embedding(nn.Module):
+    def __init__(
+        self,
+        num_embeddings: int,
+        embedding_dim: int,
+        device: torch.device | None = None,
+        dtype: torch.dtype | None = None,
+    ) -> None:
+        super().__init__()
+        self.num_embeddings = num_embeddings
+        self.embedding_dim = embedding_dim
+
+        factory_kwargs = {"device": device, "dtype": dtype}
+        self.weight = nn.Parameter(torch.empty((num_embeddings, embedding_dim), **factory_kwargs))
+        self.reset_parameters()
+
+    def reset_parameters(self) -> None:
+        nn.init.trunc_normal_(self.weight, mean=0.0, std=1.0, a=-3.0, b=3.0)
+
+    def forward(self, token_ids: torch.Tensor) -> torch.Tensor:
+        return self.weight[token_ids]
--- tests/adapters.py
+++ tests/adapters.py
@@ -7,7 +7,7 @@ from typing import IO, Any, BinaryIO
 import numpy.typing as npt
 import torch
 from cs336_basics.bpe import Tokenizer, train_bpe
-from cs336_basics.model import Linear
+from cs336_basics.model import Embedding, Linear
 from jaxtyping import Bool, Float, Int
 from torch import Tensor
 
@@ -58,8 +58,14 @@ def run_embedding(
     Returns:
         Float[Tensor, "... d_model"]: Batch of embeddings returned by your Embedding layer.
     """
-
-    raise NotImplementedError
+    embedding = Embedding(
+        num_embeddings=vocab_size,
+        embedding_dim=d_model,
+        device=weights.device,
+        dtype=weights.dtype,
+    )
+    embedding.load_state_dict({"weight": weights})
+    return embedding(token_ids)
 
 
 def run_swiglu(
